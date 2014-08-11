98ef1bcc1cb92713eb11ad7106a68e96a8901e93
Matthew Heon <mheon@redhat.com>
2014-08-11 16:02:06 -0400

    Added LGPLv2.1 license (to match libseccomp)
    Additionally added license statement and file descriptions to source files
    
    Signed-off-by: Matthew Heon <mheon@redhat.com>

--- seccomp.go
+++ seccomp.go
@@ -1,5 +1,22 @@
 // +build linux
 
+// Public API specification for libseccomp Go bindings
+// Contains public API, save filter-related functions
+
+/*
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of version 2.1 of the GNU Lesser General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses>.
+ */
+
 // Provides bindings for libseccomp, a library wrapping the Linux seccomp
 // syscall. Seccomp enables an application to restrict system call use for
 // itself and its children.
